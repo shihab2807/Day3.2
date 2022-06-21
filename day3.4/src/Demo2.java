adbe2e9b2c549fc80f65219e869dd8bedf50f786
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:01:38 +0100

    day 3 done

---
+++ day3.4/src/Demo2.java
@@ -0,0 +1,13 @@
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
