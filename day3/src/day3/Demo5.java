0d1aae535a73dca4d817218af82afac14ee06b92
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:33:33 +0100

    day3 done

---
+++ day3/src/day3/Demo5.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Demo5 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		switch (a)
+		{
+		case 1:
+			System.out.println("the value of x is 1");
+			break;
+		case 2:
+			System.out.println("the value of x is 2");
+			break;
+		case 3:
+			System.out.println("the value of x is 3");
+			break;
+		default:
+			System.out.println("the value of x is other than 1,2,3");
+			break;
+			
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
