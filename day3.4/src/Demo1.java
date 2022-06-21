6f3da89cf34102fca7083ac6fdd7c71a1d7edf22
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 08:55:47 +0100

    done

---
+++ day3.4/src/Demo1.java
@@ -0,0 +1,16 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int i = 0, j = 0;
+
+		do {
+			i++;
+			j++;
+			System.out.println("i=" +i +" and j= "  +j);
+		}
+		while ( i<10 || j<10);
+
+	}
+
+}
