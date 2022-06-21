12272436688d12ac8cd543671a11cf67dac9843b
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:37:02 +0100

    day3 done

---
+++ day3/src/day3/demo6.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class demo6 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
