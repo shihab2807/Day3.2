1273c7eda8c234b22b2fc56f4487b4f0a2158a43
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:12:18 +0100

    day3 done

---
+++ day3.4/src/Demo3.java
@@ -0,0 +1,24 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int a=5, b=10;
+		if(a<b)
+		{
+			System.out.println("a is smaller than b");
+			
+		}
+		else if (a==b)
+		{
+			System.out.println("a is equal to b");
+		
+		}
+		else 
+		{
+			System.out.println("a is greater than b");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
